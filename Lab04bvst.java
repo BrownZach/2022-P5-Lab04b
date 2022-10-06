7b3ce0c7ffaf83428166704e1d179779e7a3885f
Zach <zachary.brown@vtsd.com>
2022-10-06 13:56:18 -0400

    Start

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -13,8 +13,9 @@ public class Lab04bvst extends Applet
     public void paint(Graphics g)
     {
         // DRAW CUBE
-
-
+        g.drawRect(150, 100, 125, 125);
+        g.drawLine(150, 100, 200, 50);
+        g.drawLine( 275, 100, 325, 50);
 
         // DRAW SPHERE
 
